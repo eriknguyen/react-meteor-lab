157d2f5787924a2df401477c44644b5bfb67ad04
Erik Nguyen <namkhanh164@gmail.com>
2017-12-24 23:14:27 +0800

    url-shortener: add setup bash script

---
+++ url-shortener/setup.sh
@@ -0,0 +1,7 @@
+npm install;
+meteor add twbs:bootstrap@3.3.6;
+npm install --save react react-dom;
+meteor remove autopublish;
+meteor remove insecure;
+meteor add react-meteor-data;
+meteor npm install --save valid-url connect-route;
